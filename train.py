c406fb06e6dec394fc474a0660050eb903464e69
Justin Chiu <justinchiu@g.harvard.edu>
2017-12-21 01:20:41 -0500

    squash this into subsequent commit later

--- train.py
+++ train.py
@@ -317,6 +317,32 @@ def main():
             # Hack itos and stoi of src_vocab
             fields['src'].vocab = src_vocab
             # Since field is referenced by everything this should probably affect everything...
+    if opt.tgt_phrase_mappings:
+        # Only do source side for now, I guess
+        # We want to load the original unigram vocabulary and
+        # add on the phrase ids to the vocab afterwards, so that
+        # for all ids > N, they belong to phrases and ids < N are all unigrams
+        import pickle
+        unigram_vocab = torch.load(opt.unigram_vocab)
+        tgt_vocab = unigram_vocab[0][1]
+        unigram_size = len(tgt_vocab.itos)
+        tgt_vocab.unigram_size = unigram_size
+        def extend(vocab, word):
+            if word not in vocab.stoi:
+                vocab.stoi[word] = len(vocab.itos)
+                vocab.itos.append(word)
+        with open(opt.tgt_phrase_mappings, "rb") as f:
+            phrase_mappings = pickle.load(f)
+            phrase_mapping = {"_".join(words): list(words) for mapping in phrase_mappings for words, _ in mapping.items()}
+            for phrase, _ in phrase_mapping.items():
+                extend(tgt_vocab, phrase)
+            idx_mapping = {tgt_vocab.stoi[phrase]: list(map(lambda x: tgt_vocab.stoi[x], words)) for phrase, words in phrase_mapping.items()}
+            tgt_vocab.phrase_mapping = phrase_mapping
+            tgt_vocab.idx_mapping = idx_mapping
+            # Hack itos and stoi of tgt_vocab
+            fields['tgt'].vocab = tgt_vocab
+            # Since field is referenced by everything this should probably affect everything...
+        import pdb; pdb.set_trace()
 
     # Build model.
     model = build_model(model_opt, opt, fields, checkpoint)
