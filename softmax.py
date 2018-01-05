1d36d278ec3898ea986cbf35d9e5b99ff7074970
Justin Chiu <justinchiu@g.harvard.edu>
2018-01-04 19:58:52 -0500

    separate class and test

--- softmax.py
+++ softmax.py
@@ -6,6 +6,7 @@ from torch.nn.parameter import Parameter as P
 from torch.autograd import Variable as V
 
 from onmt.modules.Embeddings import PhraseEmbeddings
+from onmt.modules import PhrasePolytope
 
 D = 10
 nuni = 50
@@ -156,59 +157,6 @@ print(diff_right.max())
 
 # We have a dense weight as well as a sparse weight that's expensive to evaluate, so we need the concatenation of both,
 # but we only want what we need from the sparse section.
-
-class PhrasePolytope(nn.Module):
-    def __init__(self, phrase_lut, n=32):
-        super(PhrasePolytope, self).__init__()
-        self.phrase_lut = phrase_lut
-        self.nuni = phrase_lut.lut.num_embeddings
-        self.nphr = phrase_lut.phrase_vocab_size
-        self.n = n
-
-    def reset_parameters(self):
-        self.dense_lut.reset_parameters()
-        self.sparse_lut.reset_parameters()
-
-    def reset_perm(self):
-        """Using a permutation like this is probably almost equivalent to reservoir sampling"""
-        self.perm = torch.randperm(self.nphr) + self.nuni
-
-    def prepare_projection(self, target, n):
-        """ Sets the phrase vertices of the softmax projection polytope """
-        mask = target.ge(self.nuni)
-        phrases = set(target[mask])
-        distractors = self.get_distractors(n, self.perm, phrases)
-        self.vertices = sorted(list(phrases | distractors))
-        self.v2i = {v: i for i, v in enumerate(self.vertices)}
-        self.vertT = V(torch.LongTensor(self.vertices).view(-1, 1, 1))
-
-    def get_distractors(self, n, perm, phrases):
-        """ Get a set of distractors """
-        i = 0
-        d = set() 
-        while len(d) < n:
-            if not perm[i] in phrases:
-                d.add(perm[i])
-            i += 1
-        self.d = d
-        return d
-
-    def collapse_target(self, target):
-        """ Collapse Target """
-        self.prepare_projection(target, self.n)
-        self.target = target.new(list(map(
-            lambda x: self.v2i[x] + self.nuni if x > self.nuni else x,
-            target.view(-1)
-        ))).view_as(target)
-        return self.target
-
-    def forward(self, input):
-        proj = torch.cat((self.phrase_lut.lut.weight, self.phrase_lut(self.vertT).squeeze(1)), 0)
-        bias = None # for now
-        return F.linear(input, proj, bias)
-
-    def __repr__(self):
-        return "HI"
 
 pp = PhrasePolytope(phrase_lut, nd)
 pp.perm = perm_wrong
